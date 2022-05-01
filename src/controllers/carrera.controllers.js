0b65bf06405f0d836612ec7b37ae503e5c50b089
Pablo Espinosa Arias <69204800+pea0715@users.noreply.github.com>
2022-05-01 00:34:28 -0500

    crud de carrera

--- src/controllers/carrera.controllers.js
+++ src/controllers/carrera.controllers.js
@@ -1,27 +1,27 @@
 const Carrera = require("../models/carrera.models");
 
 const CreateCarrera = async (req, res) => {
-    prueba = "Aqui se Crea Carrera"
-    console.log(prueba);
-    res.send(prueba)
+    const {codigo, nombre, descripcion} = req.body;
+    const NewCarrera = new Carrera({codigo, nombre, descripcion});
+    respuesta = await NewCarrera.save();
+    res.send(respuesta)
 }
 
 const ReadCarrera = async (req, res) => {
-    prueba = "Aqui se Lee Carrera"
-    console.log(prueba);
-    res.send(prueba)
+    const respuesta = await Carrera.find({activo: true});
+    res.send(respuesta);
 }
 
 const UpdateCarrera = async (req, res) => {
-    prueba = "Aqui se Actualiza Carrera"
-    console.log(prueba);
-    res.send(prueba)
+    const { codigo, nombre, descripcion } = req.body;
+    respuesta = await Carrera.findOneAndUpdate(req.params.id, { codigo, nombre, descripcion });
+    res.send(respuesta);
 }
 
 const DeleteCarrera = async (req, res) => {
-    prueba = "Aqui se Elimina Carrera"
-    console.log(prueba);
-    res.send(prueba)
+    const { activo } = req.body;
+    respuesta = await Carrera.findOneAndUpdate(req.params.id, { activo: false })
+    res.send(respuesta);
 }
 
 module.exports = {CreateCarrera, ReadCarrera, UpdateCarrera, DeleteCarrera}
