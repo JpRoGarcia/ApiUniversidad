9d745d77d1f134bad524690f57ea569aeb9ff56e
JpRoGarcia <juan.1020405@gmail.com>
2022-05-01 22:08:14 -0500

    - Arreglo Rutas

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -10,11 +10,12 @@ const vs = "/api/v1"
 router.get(vs + '/estudiante', EstudianteController.ReadEstudiante)
     .post(vs + '/estudiante', EstudianteController.CreateEstudiante)
     .put(vs + '/estudiante/:id', EstudianteController.DeleteEstudiante)
-    .put(vs + '/estudiante/:id', EstudianteController.UpdateEstudiante)
+    .put(vs + '/estudiantes/:id', EstudianteController.UpdateEstudiante)
 
-router.get(vs + '/carrera', CarreraController.ReadCarrera)
+router.get(vs + '/carrera/:codigo', CarreraController.ReadCarrera)
+    .get(vs + '/carrera', CarreraController.ReadCarreras)
     .post(vs + '/carrera', CarreraController.CreateCarrera)
     .put(vs + '/carrera/:id', CarreraController.DeleteCarrera)
-    .put(vs + '/carrera/:id', CarreraController.UpdateCarrera)
+    .put(vs + '/carreras/:id', CarreraController.UpdateCarrera)
 
 module.exports = router
