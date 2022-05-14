86738ed70592ae25d148371de51c0dc96f85a568
JpRoGarcia <juan.1020405@gmail.com>
2022-05-13 21:38:17 -0500

    correcion routers

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -9,13 +9,13 @@ const vs = "/api/v1"
 
 router.get(vs + '/estudiante', EstudianteController.ReadEstudiante)
     .post(vs + '/estudiante', EstudianteController.CreateEstudiante)
-    .put(vs + '/estudiante/:id', EstudianteController.DeleteEstudiante)
+    .delete(vs + '/estudiante/:id', EstudianteController.DeleteEstudiante)
     .put(vs + '/estudiantes/:id', EstudianteController.UpdateEstudiante)
 
 router.get(vs + '/carrera/:codigo', CarreraController.ReadCarrera)
     .get(vs + '/carrera', CarreraController.ReadCarreras)
     .post(vs + '/carrera', CarreraController.CreateCarrera)
-    .put(vs + '/carrera/:id', CarreraController.DeleteCarrera)
+    .delete(vs + '/carrera/:id', CarreraController.DeleteCarrera)
     .put(vs + '/carreras/:id', CarreraController.UpdateCarrera)
 
 module.exports = router
